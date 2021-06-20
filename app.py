7af2536d97ccfd67f1cf4076213aa5e5d68ff2c7
Jerrod Kim <jr9597@gmail.com>
2021-06-20 14:51:27 -0700

    addedApp.run

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is our homepage'
+    return 'This is our humble homepage'
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
